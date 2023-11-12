aca367b1d55f57d838b4d1cc78d76f8903b3400a
Kathy Ems <kathy-ems@users.noreply.github.com>
2023-11-12 18:18:49 -0500

    creates run.sh for personal computer

---
+++ run_personal.sh
@@ -0,0 +1,5 @@
+source /Users/kathyems/Dropbox/git/pcc-tee-times/.venv/bin/activate
+set -o allexport
+source /Users/kathyems/Dropbox/git/pcc-tee-times/.env
+set +o allexport
+python3 /Users/kathyems/Dropbox/git/pcc-tee-times/main.py $1 $2 $3 $4 $5 $6 $7
