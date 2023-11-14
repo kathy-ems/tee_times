fb98c8abfc3b686f4bc632a940667580faccb715
Kathy Ems <kathy-ems@users.noreply.github.com>
2023-11-14 10:32:12 -0800

    updates run.sh with work computer

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source /Users/kathyle/Documents/git/personal/pcc-tee-times/.venv/bin/activate
+source /Users/kathyle/git-local/personal/pcc-tee-times/.venv/bin/activate
 set -o allexport
-source /Users/kathyle/Documents/git/personal/pcc-tee-times/.env
+source /Users/kathyle/git-local/personal/pcc-tee-times/.env
 set +o allexport
-python3 /Users/kathyle/Documents/git/personal/pcc-tee-times/main.py $1 $2 $3 $4 $5 $6 $7
+python3 /Users/kathyle/git-local/personal/pcc-tee-times/main.py $1 $2 $3 $4 $5 $6 $7
